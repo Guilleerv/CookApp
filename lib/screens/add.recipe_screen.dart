d7780ad240fa7eb4450f3f271f48226573d598ff
Guilleerv <gramosvillenas@gmail.com>
2023-12-17 20:15:28 -0300

    final

--- lib/screens/add.recipe_screen.dart
+++ lib/screens/add.recipe_screen.dart
@@ -1,144 +1,60 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'view_screnn.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-class AddRecipeScreen extends StatefulWidget {
-  @override
-  _AddRecipeScreenState createState() => _AddRecipeScreenState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _AddRecipeScreenState extends State<AddRecipeScreen> {
-  final FirebaseStorage _storage = FirebaseStorage.instance;
+class RecipesCreation extends StatefulWidget {
+  const RecipesCreation({Key? key}) : super(key: key);
+
+  @override
+  _RecipesCreationState createState() => _RecipesCreationState();
+}
+
+class _RecipesCreationState extends State<RecipesCreation> {
+  final PageController _pageController = PageController();
+  int _currentPageIndex = 0;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final _formKey = GlobalKey<FormState>();
-  late TextEditingController _titleController;
-  late TextEditingController _ingredientsController;
-  late TextEditingController _instructionsController;
-  File? _image;
-
-  @override
-  void initState() {
-    super.initState();
-    _titleController = TextEditingController();
-    _ingredientsController = TextEditingController();
-    _instructionsController = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _ingredientsController.dispose();
-    _instructionsController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _pickImage() async {
-    final ImagePicker _picker = ImagePicker();
-    try {
-      final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-      if (pickedFile != null) {
-        setState(() {
-          _image = File(pickedFile.path);
-        });
-      }
-    } catch (e) {
-      print('Error picking image: $e');
-    }
-  }
+  final TextEditingController _recipeTitleController = TextEditingController();
+  final TextEditingController _recipeDescriptionController =
+      TextEditingController();
+  final List<TextEditingController> _ingredientControllers = [
+    TextEditingController()
+  ];
+  final List<TextEditingController> _stepDescriptionControllers = [
+    TextEditingController()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Agregar Receta'),
+    return MaterialApp(
+      title: 'CookApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      home: Scaffold(
+        appBar: AppBar(
+          title:
+              const Text('Crear Receta', style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white,
+        ),
+        body: SingleChildScrollView(
           child: Column(
             children: [
-              TextFormField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: 'Título'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa un título';
-                  }
-                  return null;
-                },
+              PhotoPage(),
+              RecipeDetailsPage(
+                titleController: _recipeTitleController,
+                descriptionController: _recipeDescriptionController,
               ),
-              TextFormField(
-                controller: _ingredientsController,
-                decoration: InputDecoration(labelText: 'Ingredientes'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa los ingredientes';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _instructionsController,
-                decoration: InputDecoration(labelText: 'Instrucciones'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, ingresa las instrucciones';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              GestureDetector(
-                onTap: _pickImage,
-                child: _image != null
-                    ? kIsWeb
-                        ? Image.network(_image!.path, height: 100, width: 100)
-                        : Image.file(_image!, height: 100, width: 100)
-                    : Container(
-                        height: 100,
-                        width: 100,
-                        color: Colors.grey[300],
-                        child: Icon(Icons.camera_alt, color: Colors.black),
-                      ),
-              ),
-              SizedBox(height: 16),
+              IngredientsPage(),
+              StepsPage(),
               ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    // Validar que se haya seleccionado una imagen
-                    if (_image == null) {
-                      // Muestra un mensaje o realiza alguna acción si no se seleccionó ninguna imagen
-                      return;
-                    }
-
-                    // Upload image to Firebase Storage
-                    String imageUrl = await _uploadImage();
-                    print('Nombre de archivo: $imageUrl');
-
-                    // Add recipe data to Firestore
-                    Map<String, dynamic> newRecipe = {
-                      'nombre': _titleController.text,
-                      'ingredientes': _ingredientsController.text,
-                      'pasos': _instructionsController.text,
-                      'foto': imageUrl,
-                    };
-
-                    await _firestore.collection('receta').add(newRecipe);
-
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ViewRecetas(),
-                      ),
-                    );
-                  }
-                },
-                child: Text('Agregar Receta'),
+                onPressed: () => _saveRecipe(),
+                child: Text('Guardar Receta'),
               ),
             ],
           ),
@@ -147,17 +63,320 @@ class _AddRecipeScreenState extends State<AddRecipeScreen> {
     );
   }
 
-  Future<String> _uploadImage() async {
-    if (_image != null) {
-      String imageName = DateTime.now().millisecondsSinceEpoch.toString();
-      Reference reference =
-          _storage.ref().child('recipe_images').child('$imageName.jpg');
-      UploadTask uploadTask = reference.putFile(_image!);
-      TaskSnapshot snapshot = await uploadTask.whenComplete(() => null);
-      String downloadUrl = await snapshot.ref.getDownloadURL();
-      return downloadUrl;
-    } else {
-      return ''; // Devolver una cadena vacía si no hay imagen
+  Future<void> _saveRecipe() async {
+    String title = _recipeTitleController.text;
+    String description = _recipeDescriptionController.text;
+    List<String> ingredients =
+        _ingredientControllers.map((controller) => controller.text).toList();
+    List<String> steps = _stepDescriptionControllers
+        .map((controller) => controller.text)
+        .toList();
+
+    // Agrega líneas de impresión aquí
+    print('Ingredientes: $ingredients');
+    print('Pasos: $steps');
+
+    try {
+      await _firestore.collection('receta').add({
+        'nombre': title,
+        'descripcion': description,
+        'ingredientes': ingredients,
+        'pasos': steps,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Receta guardada con éxito'),
+      ));
+    } catch (e) {
+      print('Error al guardar la receta: $e');
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Error al guardar la receta'),
+      ));
     }
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RecipesCreation();
+  }
+}
+
+class PhotoPage extends StatefulWidget {
+  @override
+  _PhotoPageState createState() => _PhotoPageState();
+}
+
+class _PhotoPageState extends State<PhotoPage> {
+  final ImagePicker _picker = ImagePicker();
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      height: 200,
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          kIsWeb
+              ? _image != null
+                  ? Image.network(_image!.path, height: 100, width: 100)
+                  : Text('Selecciona una foto')
+              : _image != null
+                  ? Image.file(_image!, height: 100, width: 100)
+                  : Text('Selecciona una foto'),
+          ElevatedButton(
+            onPressed: _pickImage,
+            child: Text('Seleccionar Foto'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecipeDetailsPage extends StatelessWidget {
+  final TextEditingController titleController;
+  final TextEditingController descriptionController;
+
+  RecipeDetailsPage({
+    required this.titleController,
+    required this.descriptionController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _SectionTitle('Detalles de la Receta'),
+          SizedBox(height: 16),
+          TextField(
+            controller: titleController,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              labelText: 'Título',
+              labelStyle: TextStyle(color: Colors.black),
+              hintText: 'Ej: Lasaña Boloñesa.',
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: descriptionController,
+            style: TextStyle(color: Colors.black),
+            maxLines: 4,
+            decoration: InputDecoration(
+              labelText: 'Descripción',
+              labelStyle: TextStyle(color: Colors.black),
+              hintText:
+                  'Ej: Receta de rica lasaña boloñesa que me enseñó a hacer mi madre.',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SectionTitle extends StatelessWidget {
+  final String title;
+
+  const _SectionTitle(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          decoration: TextDecoration.underline,
+        ),
+      ),
+    );
+  }
+}
+
+class IngredientsPage extends StatefulWidget {
+  @override
+  _IngredientsPageState createState() => _IngredientsPageState();
+}
+
+class _IngredientsPageState extends State<IngredientsPage> {
+  final List<TextEditingController> _ingredientControllers = [
+    TextEditingController()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _SectionTitle('Ingredientes'),
+            SizedBox(height: 16),
+            for (int i = 0; i < _ingredientControllers.length; i++)
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _ingredientControllers[i],
+                          style: TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                              labelText: 'Ingrediente ${i + 1}',
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintText: 'Ej: 500 grs de carne'),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Colors.black,
+                        onPressed: () {
+                          setState(() {
+                            _ingredientControllers.removeAt(i);
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                ],
+              ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _ingredientControllers.add(TextEditingController());
+                });
+              },
+              child: Text('Agregar Ingrediente'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey[300],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StepsPage extends StatefulWidget {
+  @override
+  _StepsPageState createState() => _StepsPageState();
+}
+
+class _StepsPageState extends State<StepsPage> {
+  final ImagePicker _picker = ImagePicker();
+  final List<TextEditingController> _stepDescriptionControllers = [
+    TextEditingController()
+  ];
+  final List<File?> _stepImages = [
+    null
+  ]; // Lista para almacenar imágenes de los pasos
+
+  Future<void> _pickStepImage(int index) async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _stepImages[index] = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _SectionTitle('Pasos a Seguir'),
+            SizedBox(height: 16),
+            for (int i = 0; i < _stepDescriptionControllers.length; i++)
+              Column(
+                children: [
+                  TextField(
+                    controller: _stepDescriptionControllers[i],
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                        labelText: 'Paso ${i + 1}',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        hintText:
+                            'Ej: En una olla grande calentar el Sofrito Gourmet junto a la zanahoria, el apio y tocino (opcional).'),
+                  ),
+                  SizedBox(height: 8),
+                  _stepImages[i] != null
+                      ? kIsWeb
+                          ? Image.network(_stepImages[i]!.path,
+                              height: 100, width: 100)
+                          : Image.file(_stepImages[i]!, height: 100, width: 100)
+                      : Container(), // Mostrar imagen solo si está seleccionada
+                  ElevatedButton(
+                    onPressed: () => _pickStepImage(i),
+                    child: Text('Añadir Imagen para el Paso ${i + 1}'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.grey[300],
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Colors.black,
+                    onPressed: () {
+                      setState(() {
+                        _stepDescriptionControllers.removeAt(i);
+                        _stepImages.removeAt(i);
+                      });
+                    },
+                  ),
+                  SizedBox(height: 16),
+                ],
+              ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _stepDescriptionControllers.add(TextEditingController());
+                  _stepImages.add(null);
+                });
+              },
+              child: Text('Agregar Paso'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.grey[300],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
